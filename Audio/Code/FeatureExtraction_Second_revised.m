2da4447d3bac5b29bde4a0ed4fe20137c1f4e978
LiJunnan1992 <ljn12260409@gmail.com>
2016-05-26 17:42:04 +0800

    Add new feature_second and threshold correlation

--- Audio/Code/FeatureExtraction_Second_revised.m
+++ Audio/Code/FeatureExtraction_Second_revised.m
@@ -11,6 +11,7 @@ for iSpeaker = 1:51
     % from 1 to (kinectLen-para.lenFeature+1)
     kinectLen = floor(size(kinect,1)/25);      
     wholebody_loc_second = zeros(kinectLen,3*11); %[x1, y1, z1, x2, y2, z2, ..., xn, yn, zn]
+    wholebody_loc_std_second = zeros(kinectLen,3*11); %[x1, y1, z1, x2, y2, z2, ..., xn, yn, zn]
     %gesture_loc_second = zeros(kinectLen,3*11);    
     for iDuration = 1:kinectLen
         wholeBody_selection = (2:12);
@@ -29,16 +30,13 @@ for iSpeaker = 1:51
         
         if all(numValidFrames)            
             wholebody_loc_second(iDuration,:) = sum(wholebody_loc_second_tmp.*validFrameInd,1)./numValidFrames;
-        else
-            validFlag(iDuration) = 0;    
+            wholebody_loc_std_second(iDuration,:) = std(wholebody_loc_second,1);                     
         end
     end    
     gesture_loc_second = wholebody_loc_second(:,3*3+1:end);
+    gesture_loc_std_second = wholebody_loc_std_second(:,3*3+1:end);
 
-%     save(sprintf('./feature_second/Spk_%03d_feature_second.mat',iSpeaker),...
-%         'rate_second','energy_second','pitch_ave_second','pitch_std_second',...
-%         'sensor_second','wholebody_loc_second','gesture_loc_second','validFlag');
-                
-
+    save(sprintf('./feature_second/Spk_%03d_feature_kinect_second.mat',iSpeaker),...
+        'wholebody_loc_std_second','gesture_loc_std_second');
 %     
 end
---
+++ Audio/Code/Plot_Threshold_Corr.m
@@ -0,0 +1,51 @@
+clear;
+
+dataLoad = load('seg_corr_ana');
+corrMat = dataLoad.corrMat;
+
+spkID = 43;
+corrData = corrMat{spkID};
+
+cla;
+corrDataAna = zeros(4*2,length(corrData{1}));
+for i = 1:4
+    for j = 1:2
+        offset = (i-1)*3+j;
+        dataPlot = corrData{i,j};
+        corrDataAna((i-1)*2+j,:) = dataPlot;
+        dataPlot(dataPlot<0.1) = inf;        
+        plot(dataPlot+offset*1);
+        
+        hold on
+    end
+end
+
+thres = 0;
+corrDataAna_tmp = corrDataAna;
+corrDataAna(corrDataAna_tmp<thres) = 0;
+corrDataAna(corrDataAna_tmp>=thres) = 1;
+
+cla
+for i = 1:4
+    for j = 1:2
+        offset = (i-1)*2+j;             
+        plot(corrDataAna((i-1)*2+j,:)+offset*1.5);        
+        hold on
+    end
+end
+
+corrDataAna_sum = sum(corrDataAna,1)';
+figure
+histogram(corrDataAna_sum);
+
+
+% 
+% featureMatrix_Row(:,1) = rate(validFlag_seg>0);
+% featureMatrix_Row(:,2) = engergy(validFlag_seg>0);
+% featureMatrix_Row(:,3)  = pitch_ave(validFlag_seg>0);
+% featureMatrix_Row(:,4)  = pitch_std(validFlag_seg>0);
+% 
+% 
+% featureMatrix_Col(:,1)  = sensor_second(validFlag_seg>0);
+% featureMatrix_Col(:,2)   = wholebody(validFlag_seg>0);
+% featureMatrix_Col(:,3)  =  gesture(validFlag_seg>0);
